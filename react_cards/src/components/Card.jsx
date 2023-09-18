16e84806c8e17ebfa8a496b52670b19c308906ce
hannibal <hannibal.rabasso11@gmail.com>
2023-09-18 13:16:14 +0200

    cards

---
+++ react_cards/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+      <img src="" alt="" />
+      <div className='card-body'>
+          <h4 className='card-title'>Card Title</h4>
+            <p className='card-text text-secundary'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum distinctio molestias hic tenetur non. Quo, voluptatibus. Dolor eveniet quibusdam, a quis iste saepe ducimus nesciunt fugiat ex, repudiandae rerum quo.</p>
+            
+      </div>
+    </div>
+  )
+}
+
+export default Card
